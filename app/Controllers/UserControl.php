8b2e48276c2c0c0438c245c21c3d5c4792ef911b
rayrcreo <jjohmongcal@gmail.com>
2024-11-20 10:43:58 +0800

    fix: readded usercontrol

---
+++ app/Controllers/UserControl.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+namespace AppControllers;
+
+class Home extends BaseController
+{
+    public function index()
+    {
+        $userModel = new UserModel();
+        $data['users'] = $userModel->findAll();
+        return view('database.php',$data);
+    }
+
+    public function about()
+    {
+        return view('about');
+    }
+
+}
