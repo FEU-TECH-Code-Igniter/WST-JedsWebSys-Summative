d41b0fca4bf044749308314ee305492a2839b4c7
Chunchunmaru <thomasgabinete@gmail.com>
2024-11-20 10:28:00 +0800

    feat: Updated navbar for website

--- app/Views/components/header_nav.php
+++ app/Views/components/header_nav.php
@@ -1,10 +1,14 @@
 <header class="header">
     <!-- MISSING: LOGO -->
-    <img src="<?php echo IMG.'logo.png'; ?>" alt="Mash" class="logo" width=100px height=100px>
-    <nav class="nav">
+    <nav class="navbar bg-body-tertiary">
+        <img src="<?php echo IMG.'logo.png'; ?>" alt="Mash" class="logo" width=100px height=100px>
         <a href="<?=base_url() ?>" class="btn">Home</a>
         <a href="<?=base_url().'about' ?>" class="btn">About</a>
         <a href="<?=base_url().'content' ?>" class="btn">Content</a>
+        <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
     </nav>
     <a href="" class="btn action"></a>
 </header>
