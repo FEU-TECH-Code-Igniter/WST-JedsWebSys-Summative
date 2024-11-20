25fae2e1cf7b4b1bdba0cf8dde6531cc9dfc31b4
Chunchunmaru <thomasgabinete@gmail.com>
2024-11-20 10:45:00 +0800

    feat: Added Hero image, cards for the landing page

--- app/Views/landing.php
+++ app/Views/landing.php
@@ -1,9 +1,32 @@
 <?= $this->extend('layout/main_layout') ?>
 <?= $this->section('pageContents') ?>
-
-<h1>Jed's Web Systems</h1>
-<p>A team composed of IT students working to create web pages in accordance to their requirements in their subject "Web Systems and Development.</p>
-<button type="button" class="btn btn-dark">About</button>
-
-
-<?= $this->endSection('pageContents') ?>
+<div class="container">
+  <!-- Content here -->
+    <h1>Jed's Web Systems</h1>
+    <p>A team composed of IT students working to create web pages in accordance to their requirements in their subject "Web Systems and Development.</p>
+    <button type="button" class="btn btn-dark">About</button>
+</div>
+  <img src="https://media.istockphoto.com/id/1001279256/vector/technology-network-background.jpg?s=612x612&w=0&k=20&c=paF8I7sCkHbt2jHwRI1cpxUz2pX1F26SKl0XeugJEno=" class="img-fluid" alt="hero">
+  <div class="container">
+  <!-- Content here -->
+    <h2>Section Heading</h2>
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+    </div>
+</div>
+  <?= $this->endSection('pageContents') ?>
