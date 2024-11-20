ab57b800d8476160f0a9a27338eecdafa9255fc5
Laezi12 <jneil032803@gmail.com>
2024-11-20 09:13:43 +0800

    <feat>:  moved logo.png to public/img, implemented logo into fragments

--- app/Views/components/header_nav.php
+++ app/Views/components/header_nav.php
@@ -1,6 +1,6 @@
 <header class="header">
     <!-- MISSING: LOGO -->
-    <img src="" alt="Mash" class="logo">
+    <img src="<?php echo IMG.'logo.png'; ?>" alt="Mash" class="logo" width=100px height=100px>
     <nav class="nav">
         <a href="<?=base_url() ?>" class="btn">Home</a>
         <a href="<?=base_url().'about' ?>" class="btn">About</a>
