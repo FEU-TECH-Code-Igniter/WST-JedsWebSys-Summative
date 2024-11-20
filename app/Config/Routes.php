ffae92dec421194fd50e619bbf38f77e5a0a12d9
Laezi12 <jneil032803@gmail.com>
2024-11-20 08:53:48 +0800

    <feat>: implemented 3 fragmented pages, implemented constants, no css or design yet

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/landing', 'Home::landing');
+$routes->get('/about', 'Home::about');
+$routes->get('/content', 'Home::content');
---
+++ app/Views/components/header_nav.php
@@ -0,0 +1,10 @@
+<header class="header">
+    <!-- MISSING: LOGO -->
+    <img src="" alt="Mash" class="logo">
+    <nav class="nav">
+        <a href="<?=base_url() ?>" class="btn">Home</a>
+        <a href="<?=base_url().'about' ?>" class="btn">About</a>
+        <a href="<?=base_url().'content' ?>" class="btn">Content</a>
+    </nav>
+    <a href="" class="btn action"></a>
+</header>
